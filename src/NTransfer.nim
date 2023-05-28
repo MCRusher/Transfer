cc036f30bfb98f6e42951bbe739e658b7bb83bd6
MCRusher <Modernwarfare3Minecraft64@gmail.com>
2023-05-28 03:58:16 -0400

    Fix logging

--- src/NTransfer.nim
+++ src/NTransfer.nim
@@ -1,7 +1,7 @@
 {.experimental: "codeReordering".}
 
 import mummy, mummy/routers, mummy/fileloggers, webby
-import net, nativesockets, strformat, strutils, os, times, parseopt
+import net, nativesockets, strformat, strutils, os, times, parseopt, browsers
 
 const FilesDir = "./files" # directory files are visible/served from
 const LogsDir = "./logs" # directory where log files are stored
@@ -16,9 +16,8 @@ const DefaultPort = 80 # defaults to port 80, the default webserver port (should
 
 var router: Router # router to store all route callbacks to
 
-let log_filename = joinPath(LogsDir, $now()).addFileExt("log")
-open(log_filename, fmWrite).close() # create new log file
-var logger = newFileLogger(log_filename) # threaded logger that uses current date time as filename
+# threaded logger that uses current date time as filename
+var logger = newFileLogger(joinPath(LogsDir, now().format("yyyy-MM-dd'_'HH'h'mm'm'ss's'")).addFileExt("log"))
 
 var host = DefaultHost
 var port = DefaultPort
@@ -32,7 +31,7 @@ proc index(r: Request) =
         <html>
         <meta name='viewport' content='width=device-width, height=device-height, initial-scale=1.0'>
         <body style="background-image:linear-gradient(red, yellow); min-height: 100vh;">
-            <img src='/logo' onerror="this.src='/favicon.ico';" alt='T' style='border: thin solid white; width: 10%; height: 10%;'>
+            <img src='/logo.svg' onerror="this.src='/favicon.ico';" alt='T' style='border: thin solid white; width: 10%; height: 10%;'>
 
             <h3>Host Name: {getHostname()}</h3>
             <h3>Host Addr: {getPrimaryIPAddr()}:{port}</h3><br>
@@ -55,6 +54,7 @@ proc serveFile(r: Request) =
     let filename = joinPath(FilesDir, r.uri.parseUrl.paths[^1])
     let data = try: readFile(filename)
     except IOError:
+        logger.info(&"Failed to serve file \"{filename}\" to client \"{r.remoteAddress}\"")
         r.index() # file doesn't exist, defer to index page
         return
     
@@ -66,7 +66,7 @@ router.get("/favicon.ico") do(r: Request):
     r.respond(200, @[("Content-Type", "image/vnd.microsoft.icon")], IconData)
 
 # inline route to serve logo when asked, svg so it scales better
-router.get("/logo") do(r: Request):
+router.get("/logo.svg") do(r: Request):
     r.respond(200, @[("Content-Type", "image/svg+xml")], LogoData)
 
 # parse commandline arguments to allow overriding host and port values
@@ -81,4 +81,6 @@ for kind, key, val in getopt():
         except ValueError:
             quit("Invalid Port")
 
-newServer(router).serve(port.Port, host)
+# if the serve is listening on localhost, open the host's browser to the page automatically
+if host in [$IPv4_any(), $IPv6_any(), "127.0.0.1"]:
+    openDefaultBrowser(&"http://127.0.0.1:{port}")
